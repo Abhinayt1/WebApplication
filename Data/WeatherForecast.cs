05c930b41aabf55b103acd0c05cf2cac9a544c79
Abhinayt1 <100274657+Abhinayt1@users.noreply.github.com>
2022-02-23 17:57:15 +0530

    Update WeatherForecast.cs

--- Data/WeatherForecast.cs
+++ Data/WeatherForecast.cs
@@ -1,1 +1,16 @@
 
+using System;
+
+namespace dearazure_ga_live_demo.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
